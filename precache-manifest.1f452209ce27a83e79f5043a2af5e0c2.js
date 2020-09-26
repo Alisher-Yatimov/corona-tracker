70032c166b7a8bc1bf92db374b3bdb056c20a8e0
Alisher Yatimov <23ali0998@gmail.com>
2020-09-26 19:27:46 +0300

    Updates

---
+++ precache-manifest.1f452209ce27a83e79f5043a2af5e0c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b51ec8091731cd2f8f1554a00aadb63",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "c57f1805f4a5fb0866e0",
+    "url": "/corona-tracker/static/css/main.5160af87.chunk.css"
+  },
+  {
+    "revision": "1d59ae62c1500cbac05d",
+    "url": "/corona-tracker/static/js/2.32a0f1fe.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/corona-tracker/static/js/2.32a0f1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57f1805f4a5fb0866e0",
+    "url": "/corona-tracker/static/js/main.58abebf4.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  }
+]);
